3100776a4b46cb1265f9b35a017469af1745bfc7
Pratyush Srivastava <47223534+PratyushSr@users.noreply.github.com>
2023-02-22 15:05:11 -0600

    Wealth Watch init

---
+++ Wealth Watch VS/WealthWatch.cpp
@@ -0,0 +1,5 @@
+/* This is the main source cpp file for the project, we will run this 
+Team: Creative Minds
+Team Members: Pratyush Srivastava | Joseph Obeghe | Trudy-Ann Campbell | Esther Odesanya
+Project: Wealth Watch - Personal Financial Tracker
+*/
